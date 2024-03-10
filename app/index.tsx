979e7047f69b93d52b48e80fafbf7e39244f9990
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-03-10 19:41:06 +0800

    setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "@/components/Themed";
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
