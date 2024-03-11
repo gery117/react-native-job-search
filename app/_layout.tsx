d95cedf2153ce6d9dd9552a00c877634a64497a0
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-03-11 18:43:36 +0800

    welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import { useColorScheme } from '@/components/useColorScheme';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
-  return <Stack/>
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () =>{
+    if (fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if(!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView}/>;
 }
 
 export default Layout;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,63 @@
+import { useState } from 'react'
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 import styles from './welcome.style'
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
+
 const Welcome = () => {
+  const router = useRouter();
+
+  const [activeJobtype,setActiveJobtype]= useState('Full-time')
+  
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style = {styles.userName}>Hello Adrian</Text>
+        <Text style ={styles.welcomeMessage}>Find your perfect job</Text> 
+      </View>
+      <View style = {styles.searchContainer}>
+        <View style = {styles.searchWrapper}>
+          <TextInput 
+              style={styles.searchInput}
+              value=''
+              onChange={()=>{}}
+              placeholder='What are you looking for'
+              />
+        </View>
+
+        <TouchableOpacity style= {styles.searchBtn} onPress={()=> {}}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style = {styles.searchBtnImage}
+          />
+
+        </TouchableOpacity>
+      </View>
+
+      <View style = {styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({item})=>(
+            <TouchableOpacity 
+              style={styles.tab(activeJobtype, item)}
+              onPress ={()=>{
+                setActiveJobtype(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobtype, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View >
+
     </View>
   )
 }
