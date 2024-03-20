b41fadfe5585f86c07c2211ff385e7b8f01b82b7
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-03-20 13:52:36 +0800

    Finshed jobinfo component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -6,7 +6,7 @@ import {Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics} from 
 import {COLORS, icons, SIZES} from '../../constants';
 import useFetch from '../../hook/useFetch';
 
-
+const tabs =["About", "Qualifications","Responsibilities"]
 
 const jobDetails = () => {
     const params = useLocalSearchParams();
@@ -17,7 +17,30 @@ const jobDetails = () => {
     });
 
     const [refreshing, setRefreshing] = useState(false);
+    const[activeTab,setActiveTab] = useState(tabs[0])
+    
     const onRefresh = ()=> {}
+
+    const displayTabContent = ()=>{
+        switch(activeTab){
+            case 'Qualifications':
+                return <Specifics
+                    title="Qualifications"
+                    points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                /> 
+            case 'About':
+                return <JobAbout
+                    info={data[0].job_description?? "No data provided"}
+                />
+            case 'Responsibilities':
+                return <Specifics
+                    title="Responsibilities"
+                    points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+                /> 
+            default:
+                break;   
+        }
+    }
 
     return (
     <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightwhite}}>
@@ -60,14 +83,20 @@ const jobDetails = () => {
                         />
 
                         <JobTabs
-                        
+                            tabs={tabs}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}
                         />
+
+                        {displayTabContent()}
 
                     </View>
                 )
 
                 }
             </ScrollView>
+
+            <JobFooter url={data[0]?.job_google_link ??'https://careers.google.com/jobs/results'}/>
         </>
     </SafeAreaView>
     )
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,49 +1,49 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios';
+// import {useState, useEffect} from 'react'
+// import axios from 'axios';
 
 
-const useFetch = (endpoint, query) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+// const useFetch = (endpoint, query) => {
+//     const [data, setData] = useState([]);
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [error, setError] = useState(null);
 
-    const options = {
-            method: 'GET',
-            url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-            params: { ...query },
-            headers: {
-            'X-RapidAPI-Key': 'e8124269abmshcb92d2fa75b6976p1caf58jsn2f880e7585f0',
-            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-            }
-    };
+//     const options = {
+//             method: 'GET',
+//             url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+//             params: { ...query },
+//             headers: {
+//             'X-RapidAPI-Key': 'e8124269abmshcb92d2fa75b6976p1caf58jsn2f880e7585f0',
+//             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+//             }
+//     };
 
-    const fetchData = async ()=> {
-        setIsLoading(true);
+//     const fetchData = async ()=> {
+//         setIsLoading(true);
 
-        try{
-            const response = await axios.request(options);
-            setData(response.data.data);
-            setIsLoading(false);
-        }
-        catch(error){
-            setError(error);
-            alert('There is an error')
-        }
-        finally{
-            setIsLoading(false);
-        }
-    }
+//         try{
+//             const response = await axios.request(options);
+//             setData(response.data.data);
+//             setIsLoading(false);
+//         }
+//         catch(error){
+//             setError(error);
+//             alert('There is an error')
+//         }
+//         finally{
+//             setIsLoading(false);
+//         }
+//     }
 
-    useEffect(()=>{
-        fetchData();
-    }, [] );
+//     useEffect(()=>{
+//         fetchData();
+//     }, [] );
 
-    const refetch = ()=>{
-        setIsLoading(true);
-        fetchData();
-    }
+//     const refetch = ()=>{
+//         setIsLoading(true);
+//         fetchData();
+//     }
 
-    return{data, isLoading, error, refetch};
-}
+//     return{data, isLoading, error, refetch};
+// }
 
-export default useFetch;
+// export default useFetch;
